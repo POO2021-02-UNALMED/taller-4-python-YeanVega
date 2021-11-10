664b873da22278b52e6dc414e06042012fbc7d39
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-11-09 20:58:21 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -22,9 +22,6 @@ class Grupo:
             self.listadoAlumnos = self.listadoAlumnos + lista
         else:
             self.listadoAlumnos = lista + [alumno]
-    
-    def salida(self):
-        print("Grupo de estudiantes: ",self._grupo)
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
