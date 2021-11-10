7021a69ed3b423a3aa40fec43579c61104e88f66
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-11-09 20:52:31 -0500

    Update asignatura.py

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -3,6 +3,9 @@ class Asignatura:
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
+    
+    def __str__(self):
+        return self._nombre + " " + self._salon
 
     def salida(self):
         print(self._nombre, self._salon)
